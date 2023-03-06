78dd9a1946d42802c3fe6d82d1fe70fddf961979
Richard <richard.m.sullivan2@gmail.com>
2023-03-06 17:03:42 -0600

    made getting arguments and pulling data from the file their own functions

--- kmeans.py
+++ kmeans.py
@@ -1,31 +1,59 @@
 import sys
 
 def main():
+
+    k, input_file_name = getArgs()
+
+    data = getDataFromFile(input_file_name)
+
+    clusters = [[0,0] for i in range(k)]
+
+def getArgs():
+    # check if the right number of arguments
     argument_count = len(sys.argv)
 
     if argument_count < 3:
         print('too few arguments expected a number and a file path')
-        return
-
+        sys.exit()
     if argument_count > 3:
         print('too many arguments expected a number and a file path')
-        return
+        sys.exit()
 
-      
-    k = 0
-
+    # ckeck if k is an int
+    k = None
     try:
         k = int(sys.argv[1])
     except ValueError:
         print('first parameter was not a number')
-        return
+        sys.exit()
 
     input_file_name = str(sys.argv[2])
 
-    print(k)
-    print(input_file_name)
+    return (k,input_file_name)
 
 
+def getDataFromFile(input_file_name):
+    data = []
+
+    try:
+        input_file = open(input_file_name,'r')
+    except FileNotFoundError:
+        print('The input file was not found')
+        sys.exit()
+    except:
+        print('an file error occured')
+        sys.exit()
+    else:
+        # the cluster is assigned 0 to start
+        for i, entry in enumerate(input_file):
+            data_entry = list(map(int,entry.split()))
+            data_entry.append(0)
+
+            data.append(data_entry)
+    finally:
+        input_file.close()
+
+    return data
 
 if __name__ == '__main__':
     main()
